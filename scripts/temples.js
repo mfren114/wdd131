e60cb646ba4998673a7cc80bb12efa4b7f8a6eb1
mfren114 <mathew.frenette@yahoo.com>
2024-11-15 16:20:07 -0800

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,7 +7,7 @@ const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
 
 const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector("nav");
 
 hamButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
