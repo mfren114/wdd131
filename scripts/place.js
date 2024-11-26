7be1667d11c7e75cc3630f401815d903ec2bb18b
mfren114 <mathew.frenette@yahoo.com>
2024-11-26 11:15:33 -0800

    place

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,23 @@ document.getElementById("currentYear").textContent = currentYear;
 alert(document.lastModified);
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
+
+const temperature = 10;
+const windSpeed = 14;
+
+function calculateWindChill(temperature, windSpeed) {
+    return 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16)
+}
+
+function updateWindChill() {
+    const windChillCalculation = calculateWindChill(temperature, windSpeed);
+    const windChillElement = document.getElementById("windChillResult");
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChillElement.innerText = `${windChillCalculation.toFixed(2)}°C`;
+    } else {
+        windChillCalculation.innerText = 'N/A';
+    }
+
+    document.addEventListener(DomContentLoaded, updateWindChill);
+}
