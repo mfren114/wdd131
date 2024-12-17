b5d379eca0debadea8836037748fdaaa9ba24c0b
mfren114 <mathew.frenette@yahoo.com>
2024-12-17 11:55:18 -0800

    jazzsite

--- project/scripts/site.js
+++ project/scripts/site.js
@@ -1,3 +1,5 @@
+//hamburger button
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
@@ -6,6 +8,37 @@ menu.addEventListener('click', function () {
     menuLinks.classList.toggle('active');
 });
 
+//current date
+
 const currentYear = new Date().getFullYear();
 
 document.getElementById("currentYear").textContent = currentYear;
+
+//product array
+
+const products = [
+    {
+        id: "jb-1",
+        name: "Jazz Sauce Pan",
+    },
+    {
+        id: "jb-2",
+        name: "Jazz Utencils",
+    },
+    {
+        id: "jb-3",
+        name: "Festive Cookies",
+    },
+    {
+        id: "jb-4",
+        name: "Holiday Bread",
+    }
+];
+
+products.forEach((product) => {
+    const selection = document.getElementById('productName');
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    selection.appendChild(option);
+})
